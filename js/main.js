2af83b06add317383cf9822a7b430f2e4cafc3b8
Craig Morey <craigmorey@gmail.com>
2015-09-23 10:35:32 +0200

    Updates

--- js/main.js
+++ js/main.js
@@ -3304,6 +3304,11 @@ if (typeof define == TYPE_FUNCTION && define.amd) {
 
 })(window, document, 'Hammer');
 
+if ('addEventListener' in document) {
+    document.addEventListener('DOMContentLoaded', function() {
+        FastClick.attach(document.body);
+    }, false);
+}
 /* UTILITIES */
 
 // on ready
@@ -3650,10 +3655,6 @@ var transitionDetail = function() {
       ev.preventDefault();
 
       transitionOutProgress = true;
-
-      document.querySelectorAll('.proto__slide--hide').forEach(function(el){
-        el.classList.remove('proto__slide--hide');
-      });
     
       detailBar.classList.remove('proto__detail__bar--in');
       detailBackground.classList.remove('proto__detail__background--in');
@@ -3665,6 +3666,11 @@ var transitionDetail = function() {
       detailBody.classList.remove('proto__detail__body--in');
       detailHeader.style.webkitTransform = detailImgTransform;
       detailHeader.style.transform       = detailImgTransform;
+
+      document.querySelectorAll('.proto__slide--hide').forEach(function(el){
+        el.classList.remove('proto__slide--hide');
+      });
+
     });
 
     // end transition
